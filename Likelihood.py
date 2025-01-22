b7386f78c86397d969cc4bb7ec335be0b534a232
K.D <mylkalivor@gmail.com>
2025-01-22 14:02:44 +0100

    Courbe

--- Likelihood.py
+++ Likelihood.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import landau
+import matplotlib.pyplot as plt
 
 class LandoDistribution:
     # Distribution de Landau de paramètre µ/mu (location) et c (scale)
@@ -9,7 +10,7 @@ class LandoDistribution:
     
     def log_likelihood(self, param):
         mu,c = param
-        # Assuming Lando distribution
+        # Assuming Landau distribution
         return -(np.sum(np.log(landau.pdf(self.data, loc=mu, scale=c))))
     
     def estimate_parameter(self,init_mu, init_c):
@@ -23,9 +24,12 @@ class LandoDistribution:
 
 # Example usage
 if __name__ == "__main__":
+    loc_th = 5
+    scale_th = 3
+    Nbre = 1000
     data = []
-    for i in range(100000):
-        data.append(landau.rvs(loc=5, scale=3) )
+    for i in range(Nbre):
+        data.append(landau.rvs(loc_th, scale_th) )
     param = landau.fit(data)
     
     loc_est, scale_est = param
@@ -33,4 +37,20 @@ if __name__ == "__main__":
     
     lando_dist = LandoDistribution(data)
     estimated_param = lando_dist.estimate_parameter(1,1)
-    print(f"Paramètres estimés Likelihood (mu,c): {estimated_param:.2f}")
+    print(f"Paramètres estimés Likelihood (mu,c): {estimated_param}")
+
+    xmin = loc_th - 4*scale_th
+    xmax = loc_th + 10*scale_th
+    x = np.linspace(xmin,xmax, 1000)
+    xx = np.linspace(xmin,xmax, Nbre)
+    courbe_th = landau.pdf(x, loc=loc_th, scale=scale_th)
+    courbe_est = landau.pdf(x, loc=loc_est, scale=scale_est)
+    
+    plt.plot(x, courbe_th, 'r-', lw=2, label='Courbe théorique')
+    plt.plot(x, courbe_est, 'k--', lw=2, label='Courbe estimée')
+    plt.title('Fonction de densité de probabilité de la distribution de Landau')
+    plt.xlabel('x')
+    plt.ylabel('PDF')
+    plt.legend()
+    plt.grid(True)
+    plt.show()  
