4f2d8bbbebe5745bcc3e1e857e3ed3e65d1975ab
K.D <mylkalivor@gmail.com>
2025-01-22 14:54:36 +0100

    Last before Chabert

--- Likelihood.py
+++ Likelihood.py
@@ -21,11 +21,12 @@ class Likelihood_landau:
             return result.x[0], result.x[1] # Return estimated parameter
         else:
             raise RuntimeError("Echec")
+        
 
 if __name__ == "__main__":
     loc_th = 5
     scale_th = 3
-    Nbre = 1000
+    Nbre = 100000
     data = []
     for i in range(Nbre):
         data.append(landau.rvs(loc_th, scale_th) )
@@ -38,15 +39,20 @@ if __name__ == "__main__":
     estimated_param = lando_dist.estimate_parameter(1,1)
     print(f"Paramètres estimés Likelihood (mu,c): {estimated_param}")
 
-    xmin = loc_th - 4*scale_th
+    xmin = loc_th - 2*scale_th
     xmax = loc_th + 10*scale_th
     x = np.linspace(xmin,xmax, 1000)
     xx = np.linspace(xmin,xmax, Nbre)
     courbe_th = landau.pdf(x, loc=loc_th, scale=scale_th)
     courbe_est = landau.pdf(x, loc=loc_est, scale=scale_est)
+
+    bin_edges = np.linspace(xmin, xmax, 100)  
+    hist, edges = np.histogram(data, bins=bin_edges, density=True)  # Density = True => Normalisation or not
+    bin_centers = (edges[:-1] + edges[1:]) / 2
     
     plt.plot(x, courbe_th, 'r-', lw=2, label='Courbe théorique')
     plt.plot(x, courbe_est, 'k--', lw=2, label='Courbe estimée')
+    plt.plot(bin_centers, hist, drawstyle='steps-mid', label='Histogram-based PDF')
     plt.title('Comparaison de la courbe théorique et estimée')
     plt.xlabel('x')
     plt.ylabel('PDF')
