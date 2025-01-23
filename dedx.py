67924224824e02c224b6b3d661fd41d373a155fc
Xavier MADRE <xavier.madre@gmail.com>
2025-01-23 16:36:14 +0100

    de/dx plot en fonction de p

--- dedx.py
+++ dedx.py
@@ -19,7 +19,16 @@ with uproot.open("slim_nt_mc_aod_1.root") as file:
                 B.append(len(array[i]))
     data["Number of elements"]= B 
 
-array_dx=data['dedx_pathlength']
-array_de=data['dedx_charge']
+array_de=ak.sum(data['dedx_charge'],axis=-1)
+array_dx=ak.sum(data['dedx_pathlength'],axis=-1)
+array_p=data['track_p']
+dedx=array_de/array_dx
+print(array_p)
 
-print(array_dx[52].sum())
+# plot
+plt.scatter(array_p, dedx)
+plt.xlabel(r'p')
+plt.ylabel(r'$-\frac{dE}{dx}$)')
+plt.title('Bethe-Bloch Formula')
+plt.grid(True)
+plt.show()
