e5fb33e656c891ba8916a1720585544546a96fa1
K.D <mylkalivor@gmail.com>
2025-01-23 13:08:13 +0100

    a

--- Extraction_tab.py
+++ Extraction_tab.py
@@ -2,18 +2,20 @@ import uproot
 import pandas as pd
 import numpy as np
 
+file_name = "slim_nt_mc_aod_1.root"
 data = pd.DataFrame()
-with uproot.open("slim_nt_mc_aod_1.root") as file:
+with uproot.open(file_name) as file:
     key = file.keys()[0]  # open the first Ttree
     tree = file[key]
     # tree.show()
-    data_tree = tree.arrays(library="pd") # open data with array from numpy 
-    for array_name, array in data_tree.items(): # loop over the arrays in the tree
+    data_tree = tree.arrays(library="pd") # open data with panda 
+    for array_name, array in data_tree.items(): 
         if array_name != "nstrips":
             data[array_name] = array
         if array_name == "dedx_shape":
-            B = []
+            Nbre = []
             for i in range(len(array)):
-                B.append(len(array[i]))
-    data["Number of elements"]= B 
+                Nbre.append(len(array[i]))
+    data["Number of elements"]= Nbre
 print(data)
+
