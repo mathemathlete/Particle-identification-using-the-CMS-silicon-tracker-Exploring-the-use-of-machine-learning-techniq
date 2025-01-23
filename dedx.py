3a00e11eeaa18bbbd57c756a4a06c5cbe266e02b
Xavier MADRE <xavier.madre@gmail.com>
2025-01-23 17:21:43 +0100

    new methods for extraction data

--- dedx.py
+++ dedx.py
@@ -3,22 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import awkward as ak
 
-
+branch_of_interest = ["dedx_charge", "dedx_pathlength", "track_p"]
 data = pd.DataFrame()
 with uproot.open("slim_nt_mc_aod_1.root") as file:
     key = file.keys()[0]  # open the first Ttree
     tree = file[key]
-    # tree.show()
-    data_tree = tree.arrays(library="pd") # open data with array from numpy 
-    for array_name, array in data_tree.items(): # loop over the arrays in the tree
-        if array_name != "nstrips":
-            data[array_name] = array
-        if array_name == "dedx_shape":
-            B = []
-            for i in range(len(array)):
-                B.append(len(array[i]))
-    data["Number of elements"]= B 
-
+    data = tree.arrays(branch_of_interest, library="pd") # open data with array from numpy 
+   
 array_de=ak.sum(data['dedx_charge'],axis=-1)
 array_dx=ak.sum(data['dedx_pathlength'],axis=-1)
 array_p=data['track_p']
