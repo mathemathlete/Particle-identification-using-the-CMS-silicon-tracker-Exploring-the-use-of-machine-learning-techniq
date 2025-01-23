922beafc854ec0ea21ab9931fe3877fce40c79b5
Xavier MADRE <xavier.madre@gmail.com>
2025-01-23 10:08:15 +0100

    commentaire

--- import uproot.py
+++ import uproot.py
@@ -2,10 +2,10 @@ import uproot
 import pandas as pd
 
 with uproot.open("slim_nt_mc_aod_1.root") as file:
-    key = file.keys()[0]
-    tree = file[key]
-    data = tree.arrays(library="np")
-    for array_name, array in data.items():
+    key = file.keys()[0] # open the first Ttree
+    tree = file[key]    
+    data = tree.arrays(library="np") # open data with array from numpy 
+    for array_name, array in data.items(): # loop over the arrays in the tree
         #print("\n \n \n \n")
         print(f"Array {array_name}:")
         print(array)
