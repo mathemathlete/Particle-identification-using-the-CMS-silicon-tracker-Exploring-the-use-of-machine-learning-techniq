edae76d8b1b8cea0fc3f9fea5a285ec6a40e5f4e
xavier madre <xavier.madre@etu.unistra.fr>
2025-02-11 15:35:07 +0100

    modif file_ML

--- file_ML.py
+++ file_ML.py
@@ -12,12 +12,12 @@ branch_of_interest_LSTM = ["dedx_charge", "dedx_pathlength", "track_p","track_et
 
 # Open the ROOT file
 
-def preparation_data(file_in,file_out):
+def preparation_data(file_in,file_out,branch_of_interest):
     data = pd.DataFrame()
     with uproot.open(file_in) as file:
         key = file.keys()[0]  # open the first Ttree
         tree = file[key]
-        data = tree.arrays(branch_of_interest_LSTM, library="pd") # open data with array from numpy 
+        data = tree.arrays(branch_of_interest, library="pd") # open data with array from numpy 
 
     data_filtered = data[data['track_p'] <= 1.2 ].reset_index(drop=True) #take only particle with momentum less than 5 GeV
     data_filtered['dedx_cluster']=data_filtered['dedx_charge']/data_filtered['dedx_pathlength'] #calculate dedx and create a new column
@@ -27,6 +27,6 @@ def preparation_data(file_in,file_out):
     
 # Save the manipulated DataFrame to a new ROOT file
     with uproot.recreate(file_out) as new_file:
-        new_file["tree_name"] = { "dedx_cluster": data_filtered['dedx_cluster'], "track_p": data_filtered['track_p'],  }
+        new_file["tree_name"] = { "dedx_cluster": data_filtered['dedx_cluster'], "track_p": data_filtered['track_p'],'Ih':data_filtered['Ih'], 'track_eta': data_filtered['track_eta']  }
 
-preparation_data("tree.root","ML_training_1.2.root")
+preparation_data("tree.root","ML_training_1.2.root",branch_of_interest_LSTM)
