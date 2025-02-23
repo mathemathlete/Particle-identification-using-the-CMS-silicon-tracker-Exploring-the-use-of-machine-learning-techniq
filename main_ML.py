88468690e689cc6bdc0a4513f16a6708f8ab5064
xavier madre <xavier.madre@etu.unistra.fr>
2025-02-23 22:17:09 +0100

    ML finish

--- main_ML.py
+++ main_ML.py
@@ -16,8 +16,8 @@ import torch.optim as optim
 import pandas as pd
 
 #########################################  training/testing ML  ###############################################
-ML_train=True 
-ML_test=False
+ML_train=False 
+ML_test=True
 
 ######################################### Choose the ML model  ###############################################
 # for use the model you want you need to modify the file model and the import name line 6
@@ -25,7 +25,7 @@ time_start = rnn.timeit.default_timer()
 
 file_name = "Root_Files/ML_training_LSTM.root" # choose your data file
 branch_of_interest = ["ndedx_cluster","dedx_cluster","track_p","track_eta","Ih"]
-file_model = "Models/best_model_GRU_LSTM_150epoch_V1.pth"
+file_model_save = "Models/best_model_GRU_LSTM_150epoch_V1.pth"
 
 data=cpf.import_data(file_name,branch_of_interest)
 train_data, test_data = rnn.train_test_split(data, test_size=0.25, random_state=42)
@@ -33,8 +33,8 @@ train_data, test_data = rnn.train_test_split(data, test_size=0.25, random_state=
 # --- prepare the training data  ---
 ndedx_values_train = train_data["ndedx_cluster"].to_list()
 dedx_value_train = train_data["dedx_cluster"].to_list()
-dx_values_train = train_data["dedx_pathlength"].to_list()
-modulegeom_values_train = train_data["dedx_modulegeom"].to_list()
+# dx_values_train = train_data["dedx_pathlength"].to_list()
+# modulegeom_values_train = train_data["dedx_modulegeom"].to_list()
 data_th_values = id.bethe_bloch(938e-3, train_data["track_p"]).to_list()  # Targets (valeurs théoriques)
 p_values_train = train_data["track_p"].to_list()
 eta_values_train =  train_data["track_eta"].to_list()
@@ -44,21 +44,21 @@ Ih_values_train = train_data["Ih"].to_list()
 dataset_rnn = rnn.ParticleDataset(ndedx_values_train, dedx_value_train, data_th_values,p_values_train,eta_values_train,Ih_values_train)
 dataset_rnn2 = rnn2.ParticleDataset_V2a(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train,Ih_values_train)
 dataset_rnn3 = rnn3.ParticleDataset_V2b(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train)
-dataset_rnn4 = rnn4.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
+# dataset_rnn4 = rnn4.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
 dataset_rnn5 = rnn5.ParticleDataset_V1(ndedx_values_train, dedx_value_train, data_th_values,p_values_train,eta_values_train,Ih_values_train)
 dataset_rnn6 = rnn6.ParticleDataset_V2a(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train,Ih_values_train)
 dataset_rnn7 = rnn7.ParticleDataset_V2b(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train)
-dataset_rnn8 = rnn8.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
+# dataset_rnn8 = rnn8.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
 
 # --- prepare the dataloader ---
 dataloader_rnn = rnn.DataLoader(dataset_rnn, batch_size=32, collate_fn=rnn.collate_fn)
 dataloader_rnn2 = rnn2.DataLoader(dataset_rnn2, batch_size=32, collate_fn=rnn2.collate_fn)
 dataloader_rnn3 = rnn3.DataLoader(dataset_rnn3, batch_size=32, collate_fn=rnn3.collate_fn)
-dataloader_rnn4 = rnn4.DataLoader(dataset_rnn4, batch_size=32, collate_fn=rnn4.collate_fn)
+# dataloader_rnn4 = rnn4.DataLoader(dataset_rnn4, batch_size=32, collate_fn=rnn4.collate_fn)
 dataloader_rnn5 = rnn5.DataLoader(dataset_rnn5, batch_size=32, collate_fn=rnn5.collate_fn)
 dataloader_rnn6 = rnn6.DataLoader(dataset_rnn6, batch_size=32, collate_fn=rnn6.collate_fn)
 dataloader_rnn7 = rnn7.DataLoader(dataset_rnn7, batch_size=32, collate_fn=rnn7.collate_fn)
-dataloader_rnn8 = rnn8.DataLoader(dataset_rnn8, batch_size=32, collate_fn=rnn8.collate_fn)
+# dataloader_rnn8 = rnn8.DataLoader(dataset_rnn8, batch_size=32, collate_fn=rnn8.collate_fn)
 
 # --- prepare the test data ---
 ndedx_values_test = test_data["ndedx_cluster"].to_list()
@@ -69,24 +69,24 @@ eta_values_test =  test_data["track_eta"].to_list()
 Ih_values_test = test_data["Ih"].to_list()
 
 # --- prepare the test Particle Dataset ---
-test_dataset_rnn = rnn.ParticleDataset(ndedx_values_train, dedx_value_train, data_th_values,p_values_train,eta_values_train,Ih_values_train)
-test_dataset_rnn2 = rnn2.ParticleDataset_V2a(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train,Ih_values_train)
-test_dataset_rnn3 = rnn3.ParticleDataset_V2b(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train)
-test_dataset_rnn4 = rnn4.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
-test_dataset_rnn5 = rnn5.ParticleDataset_V1(ndedx_values_train, dedx_value_train, data_th_values,p_values_train,eta_values_train,Ih_values_train)
-test_dataset_rnn6 = rnn6.ParticleDataset_V2a(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train,Ih_values_train)
-test_dataset_rnn7 = rnn7.ParticleDataset_V2b(ndedx_values_train, dedx_value_train, data_th_values,eta_values_train)
-test_dataset_rnn8 = rnn8.ParticleDataset_V3(ndedx_values_train, dedx_value_train, dx_values_train,modulegeom_values_train, data_th_values, eta_values_train, Ih_values_train)
+test_dataset_rnn = rnn.ParticleDataset(ndedx_values_test, dedx_values_test, data_th_values_test,p_values_test,eta_values_test,Ih_values_test)
+test_dataset_rnn2 = rnn2.ParticleDataset_V2a(ndedx_values_test, dedx_values_test, data_th_values_test,eta_values_test,Ih_values_test)
+test_dataset_rnn3 = rnn3.ParticleDataset_V2b(ndedx_values_test, dedx_values_test, data_th_values_test,eta_values_test)
+# test_dataset_rnn4 = rnn4.ParticleDataset_V3(ndedx_values_test, dedx_values_test, dx_values_test,modulegeom_values_test, data_th_values_test, eta_values_test, Ih_values_test)
+test_dataset_rnn5 = rnn5.ParticleDataset_V1(ndedx_values_test, dedx_values_test, data_th_values_test,p_values_test,eta_values_test,Ih_values_test)
+test_dataset_rnn6 = rnn6.ParticleDataset_V2a(ndedx_values_test, dedx_values_test, data_th_values_test,eta_values_test,Ih_values_test)
+test_dataset_rnn7 = rnn7.ParticleDataset_V2b(ndedx_values_test, dedx_values_test, data_th_values_test,eta_values_test)
+# test_dataset_rnn8 = rnn8.ParticleDataset_V3(ndedx_values_test, dedx_values_test, dx_values_test,modulegeom_values_test, data_th_values_test, eta_values_test, Ih_values_test)
+
 
 test_dataloader_rnn = rnn.DataLoader(test_dataset_rnn, batch_size=32, collate_fn=rnn.collate_fn)
 test_dataloader_rnn2 = rnn2.DataLoader(test_dataset_rnn2, batch_size=32, collate_fn=rnn2.collate_fn)
 test_dataloader_rnn3 = rnn3.DataLoader(test_dataset_rnn3, batch_size=32, collate_fn=rnn3.collate_fn)
-test_dataloader_rnn4 = rnn4.DataLoader(test_dataset_rnn4, batch_size=32, collate_fn=rnn4.collate_fn)
+# test_dataloader_rnn4 = rnn4.DataLoader(test_dataset_rnn4, batch_size=32, collate_fn=rnn4.collate_fn)
 test_dataloader_rnn5 = rnn5.DataLoader(test_dataset_rnn5, batch_size=32, collate_fn=rnn5.collate_fn)
 test_dataloader_rnn6 = rnn6.DataLoader(test_dataset_rnn6, batch_size=32, collate_fn=rnn6.collate_fn)
 test_dataloader_rnn7 = rnn7.DataLoader(test_dataset_rnn7, batch_size=32, collate_fn=rnn7.collate_fn)
-test_dataloader_rnn8 = rnn8.DataLoader(test_dataset_rnn8, batch_size=32, collate_fn=rnn8.collate_fn)
-
+# test_dataloader_rnn8 = rnn8.DataLoader(test_dataset_rnn8, batch_size=32, collate_fn=rnn8.collate_fn)
 
 
 
@@ -113,7 +113,7 @@ dropout_MLP = 0.1
 dropout_dedx = 0.1
 
 # Number of epochs
-epoch = 1
+epoch = 150
 
 model = rnn.LSTMModel(dedx_hidden_size, dedx_num_layers, lstm_hidden_size, lstm_num_layers, dropout_GRU, dropout_dedx, dropout_LSTM, adjustement_scale)
 model2 = rnn2.LSTM_V2a(dedx_hidden_size, dedx_num_layers, lstm_hidden_size, lstm_num_layers, dropout_GRU, dropout_dedx, dropout_LSTM, adjustement_scale)
@@ -137,11 +137,11 @@ scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience
 # --- Testing model ---
 if ML_train==True:
     print("Training model...")
-    ML.loss_epoch(rnn.start_ML(model,file_model_save,dataloader_rnn,criterion,epoch, True, False))
+    ML.loss_epoch(rnn.start_ML(model,file_model_save,dataloader_rnn,criterion,epoch, True, False)) #choose the dataloader you want to use
 
 if ML_test==True:
     print("Testing model...")
-    predictions, test_loss = rnn.start_ML(model,file_model_save,test_dataloader_rnn,criterion,epoch, False, True)
+    predictions, test_loss = rnn.start_ML(model,file_model_save,test_dataloader_rnn,criterion,epoch, False, True) #choose the test_dataloader you want to use
 
 
 time_end = rnn.timeit.default_timer()
@@ -153,17 +153,17 @@ print(f"Execution time: {elapsed_time:.2f} seconds ({int(hours)} h {int(minutes)
 ######################################### Part where we plot data from ML ###############################################
 
 data_plot=pd.DataFrame()
-data_plot['track_p']=test_dataset_rnn["track_p"].to_list()
+data_plot['track_p']=p_values_test
 data_plot['dedx']=predictions
 data_plot['Ih']=Ih_values_test
 data_plot['Ih']=data_plot['Ih']*1e-3
 data_plot['track_eta']=test_data['track_eta']
 
-ylim_plot=[2,9]
-ML.plot_ML(data_plot,ylim_plot, True,True, True)
+ylim_plot=[0,9]
+ML.plot_ML(data_plot,ylim_plot, False,False, False)
 #ML.plot_ratio(data_plot,id.m_p)  
 ML.density(data_plot,15,ylim_plot)
-ML.std(data_plot,15,True)
-ML.biais(data_plot,"track_eta",15)
-ML.biais(data_plot,"track_p",15)
+#ML.std(data_plot,15,True)
+#ML.biais(data_plot,"track_eta",15)
+#ML.biais(data_plot,"track_p",15)
 
