5e201af33e43e94983b04584b02b388a7f95e61f
K.D <mylkalivor@gmail.com>
2025-02-23 11:02:44 +0100

    a

--- file_ML.py
+++ file_ML.py
@@ -51,7 +51,7 @@ def preparation_data2(data,file_out,branch_of_interest_out,p_min,p_max,Ih_max):
     data_filtered = data_filtered[data_filtered['track_p'] >= p_max].reset_index(drop=True) #take only particle with momentum more than pmax GeV
     data_filtered['dedx_cluster']=data_filtered['dedx_charge']/data_filtered['dedx_pathlength'] #calculate dedx and create a new column
     data_filtered['Ih'] = np.sqrt(ak.sum(data_filtered['dedx_cluster']**2, axis=-1) / ak.count(data_filtered['dedx_cluster'], axis=-1)) #calculate quadratique mean of dedx along a track
-    data_filtered=data_filtered[data_filtered['Ih'] <= Ih_max].reset_index(drop=True) #Premier filtrage sur les données dedx
+    data_filtered= data_filtered[data_filtered['Ih'] <= Ih_max].reset_index(drop=True) #Premier filtrage sur les données dedx
     data_filtered = data_filtered[(data_filtered['Ih'] >= id.bethe_bloch(mass_limit, data_filtered['track_p']) * scaling)] #Filtrage du bruit 
 # Save the manipulated DataFrame to a new ROOT file
     with uproot.recreate(file_out) as new_file:
